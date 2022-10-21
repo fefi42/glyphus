0d71541969455219a82d04c58c345ff01475ca5e
Felix Finkbeiner <ffinkbeiner@visualfabriq.com>
2022-10-21 16:30:43 +0200

    Add error return value

--- src/cli.rs
+++ src/cli.rs
@@ -2,8 +2,11 @@ pub struct Config {
     file_path: String
 }
 
-pub fn parse_args(args: &[String])-> Config {
-    Config { file_path: args[1].clone() }
+pub fn parse_args(args: &[String])-> Result<Config, &'static str> {
+    if args.len() < 2 {
+        return Err("no input file path provided")
+    }
+    Ok(Config { file_path: args[1].clone() })
 }
 
 
@@ -13,9 +16,18 @@ mod tests {
 
     #[test]
     fn test_parse_args() {
-
         let args: Vec<String> = vec![String::from("target/program"), String::from("test_folder/my_picture.png")];
-        let cfg = parse_args(&args);
+        let cfg = parse_args(&args).unwrap();
         assert_eq!(cfg.file_path, "test_folder/my_picture.png");
     }
+
+    #[test]
+    fn no_arguments() {
+        let args: Vec<String> = Vec::new();
+        match parse_args(&args) {
+            Ok(i) => assert!(false, "Expected function to fail due to missing required arguments"),
+            Err(e) => assert_eq!(e, "no input file path provided"),
+        }
+    }
+
 }
